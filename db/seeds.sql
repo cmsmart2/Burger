75a3c4a0a7f94881203b1aa74b2f06e8c970e5dc
Cera Smart <cera_smart@yahoo.com>
2019-12-01 16:24:46 -0600

    added to schema and seeds for mysql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Inserted a set of records into the table
+INSERT INTO menu (burger_name)
+VALUES ("Original");
+
+INSERT INTO menu (burger_name)
+VALUES ("Original with Bacon");
+
+INSERT INTO menu (burger_name)
+VALUES ("Chicken");
+
+INSERT INTO menu (burger_name)
+VALUES ("Double cheese");
