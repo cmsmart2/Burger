f4bf706bf47676bee970fae355ac8b614e7d1b69
Cera Smart <cera_smart@yahoo.com>
2019-12-08 13:17:57 -0600

    worked on orm and server files

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,54 @@
 const db = require("./connection.js");
-const orm = {
-    select: function(table){
-        let queryString = "SELECT * FROM ??";
-        db.query(queryString, [table], function(err, result){
-          if(err) throw err;
-          console.log(result);  
-        })
+// Object for all our SQL statement functions.
+var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    
+    update: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
     }
-}
-module.exports = orm;
+  };
+  
+  // Export the orm object for model
+  module.exports = orm;
