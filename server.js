928d11ef57041856c9ac6aba714bcdc7c920805c
Cera Smart <cera_smart@yahoo.com>
2019-12-14 23:51:54 -0600

    added some style and images

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
-
+const path = require("path")
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
