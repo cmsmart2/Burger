8d63335a371c0789f01afe53ea247fddf6a02f18
Cera Smart <cera_smart@yahoo.com>
2019-12-15 19:51:22 -0600

    trying to make work on heroku

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".devour").on("click", function(event) {
+    $(".devour").on("submit", function(event) {
       event.preventDefault();
 
       var id = $(this).children(".id").val();
